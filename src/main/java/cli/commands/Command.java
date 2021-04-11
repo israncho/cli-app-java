c8fdced8793ee33b0ff49023f8a89719be326721
Israel Gutiérrez <isragutierrez@ciencias.unam.mx>
2021-04-11 17:40:55 -0500

    version 1.1 doc fixed and extra method

--- src/main/java/cli/commands/Command.java
+++ src/main/java/cli/commands/Command.java
@@ -1,24 +1,26 @@
 package cli.commands;
 
+import java.util.LinkedList;
+
 /**
  * Interface that models the behavior of the commands of a command line
  * interface.
  * 
  * @author Jesus Israel Gutierrez Elizalde.
- * @version 1.0
+ * @version 1.1
  */
 public interface Command {
 
     /**
-     * Performs the action of the command.
+     * Performs the action of the command. Some commands have extra usage options.
      * 
-     * @param parameters -- Parameters of the command.
+     * @param options -- Extra usage options of the command.
      */
-    public void action(String[] parameters);
+    public void action(LinkedList<String> options);
 
     /**
-     * Returns the name of the command. Interface that models the behavior of the
-     * commands of a command line interface.
+     * Returns the name of the command. The name of the command is also the id, so
+     * two different commands cannot have the same name.
      * 
      * @return String -- Name of the command.
      */
@@ -30,4 +32,12 @@ public interface Command {
      * @return String -- The manual of the command.
      */
     public String manual();
+
+    /**
+     * Returns the message for wrong options of the command. Must be used when the
+     * input for the command is not quite correct.
+     * 
+     * @return String -- Message for wrong options of the command.
+     */
+    public String wrongOptions();
 }
