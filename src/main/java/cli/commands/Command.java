4b4ea6bf789fc3387eb8f6d12eff13c0fd6e5437
Israel Gutiérrez <isragutierrez@ciencias.unam.mx>
2021-04-11 21:55:36 -0500

    1.1.1 just better doc

--- src/main/java/cli/commands/Command.java
+++ src/main/java/cli/commands/Command.java
@@ -7,35 +7,35 @@ import java.util.LinkedList;
  * interface.
  * 
  * @author Jesus Israel Gutierrez Elizalde.
- * @version 1.1
+ * @version 1.1.1
  */
 public interface Command {
 
     /**
-     * Performs the action of the command. Some commands have extra usage options.
+     * Performs the action of this command. Some commands have extra usage options.
      * 
      * @param options -- Extra usage options of the command.
      */
     public void action(LinkedList<String> options);
 
     /**
-     * Returns the name of the command. The name of the command is also the id, so
+     * Returns the name of this command. The name of the command is also the id, so
      * two different commands cannot have the same name.
      * 
-     * @return String -- Name of the command.
+     * @return String -- Name of this command.
      */
     public String name();
 
     /**
-     * Returns the manual of the command; description of how to use the command.
+     * Returns the manual of this command; description of how to use the command.
      * 
-     * @return String -- The manual of the command.
+     * @return String -- The manual of this command.
      */
     public String manual();
 
     /**
-     * Returns the message for wrong options of the command. Must be used when the
-     * input for the command is not quite correct.
+     * Returns the message for wrong options of this command. Must be used when the
+     * input options for this command are not quite correct.
      * 
      * @return String -- Message for wrong options of the command.
      */
