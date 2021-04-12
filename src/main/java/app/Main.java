dea03238baf4af723936b3567b9874b31f52b6df
Israel Gutiérrez <isragutierrez@ciencias.unam.mx>
2021-04-12 01:56:44 -0500

    main class run the program

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,15 @@
+package app;
+
+import cli.Cli;
+
+/**
+ * Main class that runs the program.
+ * 
+ * @author Jesús Israel Gutiérrez Elizalde.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Cli cli = new Cli();
+        cli.run();
+    }
+}
